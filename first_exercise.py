1e1b9b4bd03f34fe80fd7b448b12ab2a648a30c9
Evgeniy Egorov <egorovzhenya@gmail.com>
2021-10-21 16:14:44 +0400

    Function added

---
+++ first_exercise.py
@@ -0,0 +1,18 @@
+from pprint import pprint
+
+with open('recipes.txt', mode='r',  encoding='utf-8-sig') as file:
+    cook_book = {}
+    for dish in file:
+        dish_name = dish.strip()
+        ingredients_qty = int(file.readline().strip())
+        ingredients = []
+        for els in range(ingredients_qty):
+            name, qty, measure = file.readline().split('|')
+            ingredient = {'name': name.strip(), 'qty': qty.strip(), 'measure': measure.strip()}
+            ingredients.append(ingredient)
+        cook_book[dish_name] = ingredients
+        file.readline()
+    pprint(cook_book, sort_dicts=False)
+
+
+
